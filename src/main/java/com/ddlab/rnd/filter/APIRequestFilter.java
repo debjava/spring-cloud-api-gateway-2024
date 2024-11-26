c2f7bfaae823fbd84501f489214a3918ddc30b86
debjava <debajava>
2024-11-26 16:13:54 +0530

    Code committed and pushed by debajava on 26-Nov-2024 04:13 pm using DDLAB CodePusblisher tool

---
+++ src/main/java/com/ddlab/rnd/filter/APIRequestFilter.java
@@ -0,0 +1,38 @@
+package com.ddlab.rnd.filter;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class APIRequestFilter implements GlobalFilter {
+	
+	@Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        log.debug("Incoming Request URI: {}", originalUri);
+        log.debug("Application Id: {}", route.getId());
+        log.debug("Routed URI: {}", routeUri);
+        log.debug("Incoming Request Http Method: {}", exchange.getRequest().getMethod());
+        
+        return chain.filter(exchange);
+    }
+
+}
